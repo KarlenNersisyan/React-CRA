8a4264486126f6672616673cd5ae613746983afb
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-13 00:24:27 +0400

    fix(Index): Changes in file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Weather Forecast</h1>,
+    <h1>Weather Forecast</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
